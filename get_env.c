60256ddcedcd3e5442402b595f9df6e234de5c79
william maina <willima09@gmail.com>
2023-12-13 06:15:51 -0800

    b

---
+++ get_env.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+
+/**
+ * c_strdup - custom string duplication; excludes beginning bytes
+ * @str: string to duplicate (e.g. environmental variable PATH=/bin:/bin/ls)
+ * @cs: number of bytes to exclude (e.g. excludes "PATH=")
+ * 
+ * Return: string (e.g. /bin:/bin/ls)
+ */
+
+char *c_strdup(char *str, int cs)
+{
+	char *duplicate_str;
+	int i, len = 0;
+
+	if (str == NULL) 
+		return (NULL);
+
+	while (*(str + len))
+		len++;
+	len++;
+
+	duplicate_str = malloc(sizeof(char) * (len - cs));
+	if (duplicate_str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < (len - cs))
+	{
+		*(duplicate_str + i) = *(str + cs + i);
+		i++;
+	}
+	return (duplicate_str);
+}
+
+/**
+ * get_env - finds and returns a copy of the requested environmental variable
+ * @str: string to store it in
+ * @env: entire set of environmental variables
+ * 
+ * Return: copy of requested environmental variable
+ */
+
+char *get_env(char *str, list_t *env)
+{
+	int j = 0, cs = 0;
+
+	while (env != NULL)
+	{
+		j = 0;
+		while ((env->var)[j] == str[j]) 
+			j++;
+		if (str[j] == '\0' && (env->var)[j] == '=')
+			break;
+		env = env->next;
+	}
+
+	while (str[cs] != '\0') 
+		cs++;
+	cs++; 
+	return (c_strdup(env->var, cs)); 
+}
